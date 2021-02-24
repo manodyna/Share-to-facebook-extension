a3cc3781a59936ef96e4a6a7d191aef734f86269
manodyna <77208955+manodyna@users.noreply.github.com>
2021-02-24 11:27:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 console.log("my extension");
 
+// getting all links on the page by tag name
 var links = document.getElementsByTagName("a")
 var formattedLinks = [];
 
+// addding links to formatted links array
 for(let i=0; i<links.length; i++){
     let title = links[i].text;
     let href = links[i].href; 
